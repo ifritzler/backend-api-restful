63f979dad8f3bcb63f9b818de525950d857c77a0
Ilan Fritzler <contacto.fritzlerilan@gmail.com>
2022-10-20 21:15:27 -0300

    products initial state

--- src/routes/products/products.router.js
+++ src/routes/products/products.router.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+// Simula una base de datos en memoria
+const initialState = {
+  products: [],
+  rowCount: 0,
+};
+
 router.get("/", (req, res) => {
   res.status(200).json({ message: "products router" });
 });
