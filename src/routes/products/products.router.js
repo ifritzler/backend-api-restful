e17dfab3c85a2713f0c94bd796abe117648ab68d
Ilan Fritzler <contacto.fritzlerilan@gmail.com>
2022-10-21 02:43:46 -0300

    add product service

--- src/routes/products/products.router.js
+++ src/routes/products/products.router.js
@@ -3,67 +3,52 @@ const {
   ProductNotFoundException,
   ProductEmptyEntity,
 } = require("../../exceptions");
+const ProductService = require("../../services/products/products.service");
+
 const router = express.Router();
+const productService = new ProductService();
+// Servicio de productos injectado en el objeto request, lo hace global a cada request
+router.use((req, res, next) => {
+  req.service = productService;
+  next();
+});
 
-// Simula una base de datos en memoria
-const state = {
-  products: [],
-  rowCount: 0,
-};
-
-router.get("/", (_req, res) => {
-  res.status(200).json({ data: state.products });
+router.get("/", (req, res) => {
+  const data = req.service.getAll();
+  res.status(200).json({ data });
 });
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-
-  const product = state.products.find((item) => item.id == id);
-
-  if (!product) throw new ProductNotFoundException();
-
+  const product = req.service.getById(id);
   res.status(200).json({ data: product });
 });
 
 router.post("/", (req, res) => {
   const product = req.body;
-  // TODO: Aqui deberia haber uan validacion mas efectiva que solo verificar que no haya llegado nada
+  // TODO: Esta validacion deberia ser un middleware en un futuro con un DTO. Probablemente con class-validator
   if (Object.entries(product).length === 0) throw new ProductEmptyEntity();
 
-  // Seteamos el nuevo id y guardamos en la base de datos
-  state.rowCount += 1;
-  const newProduct = { id: state.rowCount, ...product };
-  state.products.push(newProduct);
+  const created = req.service.create(product);
 
   // Devolvemos la respuesta satisfactoria
-  res.status(201).json(newProduct);
+  res.status(201).json(created);
 });
 
 router.put("/:id", (req, res) => {
   const { id } = req.params;
   const changes = req.body;
-  // TODO: Aqui deberia haber uan validacion mas efectiva que solo verificar que no haya llegado nada
+  // TODO: Esta validacion deberia ser un middleware en un futuro con un DTO. Probablemente con class-validator
   if (Object.entries(changes).length === 0) throw new ProductEmptyEntity();
 
-  // Modifico el producto en cuestion dentro de la base de datos
-  state.products = state.products.map((product) => {
-    if (product.id === parseInt(id)) {
-      product = { ...product, ...changes };
-    }
-    return product;
-  });
-
+  req.service.putById(id, changes);
   // Devolvemos la respuesta satisfactoria
   res.status(204).send();
 });
 
 router.delete("/:id", (req, res) => {
   const { id } = req.params;
-
-  state.products = state.products.filter(
-    (product) => product.id !== parseInt(id)
-  );
-
+  req.service.deleteById(id);
   res.status(204).send();
 });
 
