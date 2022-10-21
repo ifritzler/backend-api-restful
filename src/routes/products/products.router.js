92dc7d7bbfa41ac718a4360d4ab1bd2c31d0aa15
Ilan Fritzler <contacto.fritzlerilan@gmail.com>
2022-10-20 21:47:33 -0300

    add get by id product endpoint

--- src/routes/products/products.router.js
+++ src/routes/products/products.router.js
@@ -1,14 +1,24 @@
 const express = require("express");
+const { ProductNotFoundException } = require("../../exceptions");
 const router = express.Router();
 
 // Simula una base de datos en memoria
-const initialState = {
+const state = {
   products: [],
   rowCount: 0,
 };
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "products router" });
+router.get("/", (_req, res) => {
+  res.status(200).json({ data: state.products });
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params.id;
+  const product = state.products.find((item) => item.id === id);
+
+  if (!product) throw new ProductNotFoundException();
+
+  res.status(200).json({ data: product });
 });
 
 module.exports = router;
